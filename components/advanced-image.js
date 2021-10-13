4844bb5b1cd47ba2838e3bd530cef8996cf4b5a1
bobbywan-kenoby <leonoixdecoco@gmail.com>
2021-10-13 13:38:20 +0200

    event on component main div: better renderer
    v-if debug: suppr unecessary DOM element

--- components/advanced-image.js
+++ components/advanced-image.js
@@ -14,67 +14,73 @@ app.component('advanced-image', {
         }
     },
     data() {
-        return {
-			imageFocus: false,
-			x: 0,
-			y: 0,
-			scrollX: 0,
-			scrollY: 0
-		};
+        return {imageFocus: false, x: 0, y: 0, scrollX: 0, scrollY: 0};
     },
     template: `
-    <div class="advanced-image" :style="{padding: (canvas_size/2)+'px', borderRadius: (canvas_size/2)+'px'}">
+    <div class="advanced-image" :style="{padding: (canvas_size/2)+'px', borderRadius: (canvas_size/2)+'px'}" @mousemove="getMousePos($event)" @mouseenter="imageFocus=true" @mouseleave="imageFocus=false">
         <canvas :id="name" class="inspect-image" :style="canvasStyle" v-if="imageFocus"></canvas>
         <img :id="name+'-source'" v-show="false" src="./assets/images/Mozilla_Firefox_anime_girls_traditional_clothing_kimono_anime_V3-2.png">
-        <img :style="imageStyle" src="./assets/images/firefox_logo_2017.png" @mousemove="getMousePos($event)" @mouseenter="imageFocus=true" @mouseleave="imageFocus=false">
-		<p v-show="false">{{ debug }}</p>
+        <img :style="imageStyle" src="./assets/images/firefox_logo_2017.png">
+		<p v-if="false">{{ debug }}</p>
     </div>`,
     methods: {
         draw() {
             let image = document.getElementById(`${this.name}-source`);
-            let fa = this.image_size / (
-                image.width > image.height
-                ? image.width
-                : image.height);
 
             let source = document.createElement('canvas');
             source.width = this.canvas_size + this.image_size;
             source.height = this.canvas_size + this.image_size;
 
             let ctx = source.getContext('2d');
-			ctx.fillStyle = 'white';
-			ctx.fillRect(0, 0, source.width, source.height);
-            ctx.drawImage(image, this.canvas_size/2, this.canvas_size/2, this.image_size, this.image_size);
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, source.width, source.height);
+            ctx.drawImage(image, this.canvas_size / 2, this.canvas_size / 2, this.image_size, this.image_size);
 
             let canvas = document.getElementById(this.name);
-			canvas.width = this.canvas_size;
+            canvas.width = this.canvas_size;
             canvas.height = this.canvas_size;
 
             ctx = canvas.getContext('2d');
-            ctx.drawImage(source, this.x, this.y, this.canvas_size, this.canvas_size, 0, 0, this.canvas_size, this.canvas_size);
+            ctx.drawImage(source, this.x - this.canvas_size / 2, this.y - this.canvas_size / 2, this.canvas_size, this.canvas_size, 0, 0, this.canvas_size, this.canvas_size);
         },
         getMousePos(event) {
-			console.log(event);
+            console.log(event);
             let rect = event.target.getBoundingClientRect();
             this.x = event.clientX - rect.left;
+            this.x = this.x <= this.canvas_size / 2
+                ? this.canvas_size / 2
+                : this.x;
+            this.x = this.x >= this.image_size + this.canvas_size / 2
+                ? this.image_size + this.canvas_size / 2
+                : this.x;
             this.y = event.clientY - rect.top;
-            this.scrollX = event.target.offsetLeft - this.canvas_size/2;
-            this.scrollY = event.target.offsetTop - this.canvas_size/2;
+            this.y = this.y <= this.canvas_size / 2
+                ? this.canvas_size / 2
+                : this.y;
+            this.y = this.y >= this.image_size + this.canvas_size / 2
+                ? this.image_size + this.canvas_size / 2
+                : this.y;
+            this.scrollX = event.target.offsetLeft - this.canvas_size / 2;
+            this.scrollY = event.target.offsetTop - this.canvas_size / 2;
 
-			this.draw()
+            this.draw()
         }
     },
     computed: {
         canvasStyle() {
             return {
                 top: `${this.y + this.scrollY}px`,
-                left: `${this.x  + this.scrollX}px`,
+                left: `${this.x + this.scrollX}px`,
                 width: `${this.canvas_size}px`,
                 height: `${this.canvas_size}px`
             };
         },
         imageStyle() {
-            return {width: `${this.image_size}px`, height: `${this.image_size}px`};
+            return {
+                width: `${this.image_size}px`,
+                height: `${this.image_size}px`,
+                pointerEvents: "none"
+            };
         },
         debug() {
             return this.imageFocus
